2391583702e5271cc87a2eb44b7f2600063803ef
ChrisAkridas <xrhstosakridas@gmail.com>
2025-04-06 23:35:35 +0300

    feat: add python script for train/predict/evaluate BKT model

--- src/lib/scripts/python/bkt.py
+++ src/lib/scripts/python/bkt.py
@@ -7,25 +7,26 @@ from pyBKT.models import Model
 
 my_path = os.path.dirname(__file__)
 
-# def run_bkt(skills):
-#     model = Model()
-#     model.fit(data_path="../../training.csv")  # Replace with actual dataset
-#     predictions = model.predict(data_path="example_test.csv", skills=skills)
-#     return predictions.tolist()
+def train_pybkt_model(): 
+    model = Model()
+    model.fit(data_path= my_path + "\\trainingDataset.csv")
+
+def save_model(model, file_path): 
+    model.save(file_path)
+
+def load_model(file_path):
+    return Model.load(file_path)
+
+def run_bkt(skills):
+    model = Model()
+    model.fit(data_path = my_path + '\\trainingDataset.csv', skills=skills)  # Replace with actual dataset
+    predictions = model.predict(data_path = my_path + '\\trainingDataset.csv')
+    evaluations = model.evaluate(data_path = my_path + '\\trainingDataset.csv')
+    return {"predictions": predictions, "evaluations": evaluations}
 
 if __name__ == "__main__":
     input_data = json.loads(sys.argv[1])
     skills = input_data.get('skills')
-  
-    # print(my_path + '\\training.csv')
-  
-    model = Model()
-    model.fit(data_path = my_path + '\\training.csv')  # Replace with actual dataset
-    # print(model.params())
-    # predictions = model.predict(data_path="example_test.csv", skills=skills)
-    # return predictions.tolist()
-    # print(json.dumps({"predictions": predictions.tolist()}))
-
-    # df = pd.read_csv("../../data_sets/training.csv")  # Replace with actual dataset
-    # result = run_bkt(skills)
-    # print(json.dumps({"predictions": result}))
+    
+    result = run_bkt(skills)
+    print(result)
