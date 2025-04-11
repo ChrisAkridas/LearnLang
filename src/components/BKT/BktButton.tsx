ccb8a53686ffbeb3e923c3f6a8730ed742d1d771
ChrisAkridas <xrhstosakridas@gmail.com>
2025-04-12 00:46:33 +0300

    feat: some changes

---
+++ src/components/BKT/BktButton.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { ComponentProps } from "react";
+import { Button } from "../ui/Button";
+import { BKTRouteBody } from "@/types/types";
+
+interface Props extends ComponentProps<typeof Button> {}
+export default function BktButton(props: Props) {
+  return (
+    <Button
+      onClick={async () => {
+        const response2 = await fetch("/api/update_dataset", {
+          method: "POST",
+          body: JSON.stringify({
+            data: [
+              {
+                user_id: 1,
+                skill_name: "vocabulary",
+                correct: 1,
+                problem_id: "02ef263b-b093-4af2-ae2f-28e070b5ed32_mult",
+                duration: 10.5,
+                response_text: "Εγώ",
+                resource: "I",
+              },
+            ],
+            // filename: "chrisData.csv",
+          } as BKTRouteBody),
+        });
+        console.log("result2: ", await response2.json());
+        const response = await fetch("/api/bkt", {
+          method: "POST",
+          body: JSON.stringify({
+            // skills: ["vocabulary"],
+            data: [
+              {
+                user_id: 1,
+                skill_name: "vocabulary",
+                correct: 1,
+                problem_id: "02ef263b-b093-4af2-ae2f-28e070b5ed32_mult",
+                duration: 10.5,
+                response_text: "Εγώ",
+                resource: "I",
+              },
+            ],
+            filename: "chrisData.csv",
+          } as BKTRouteBody),
+        });
+        // console.log("response: ", response);
+        console.log("result: ", await response.json());
+      }}
+      {...props}
+    />
+  );
+}
--- src/lib/scripts/python/bkt.py
+++ src/lib/scripts/python/bkt.py
@@ -17,16 +17,23 @@ def save_model(model, file_path):
 def load_model(file_path):
     return Model.load(file_path)
 
-def run_bkt(skills):
+def train_model():
     model = Model()
-    model.fit(data_path = my_path + '\\trainingDataset.csv', skills=skills)  # Replace with actual dataset
-    predictions = model.predict(data_path = my_path + '\\trainingDataset.csv')
-    evaluations = model.evaluate(data_path = my_path + '\\trainingDataset.csv')
+    model.fit(data_path=my_path + "\\trainingDataset.csv")
+    return model
+
+def run_bkt():
+    model = Model()
+    
+    model.fit(data_path = my_path + '\\trainingDataset.csv',)  # Replace with actual dataset
+    save_model(model, my_path + '\\model.pkl')
+    predictions = model.predict(data_path = my_path + '\\chrisData.csv')
+    evaluations = model.evaluate(data_path = my_path + '\\chrisData.csv')
     return {"predictions": predictions, "evaluations": evaluations}
 
 if __name__ == "__main__":
     input_data = json.loads(sys.argv[1])
-    skills = input_data.get('skills')
+    # skills = input_data.get('skills')
     
-    result = run_bkt(skills)
+    result = run_bkt()
     print(result)
