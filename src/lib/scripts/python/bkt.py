1550cc3b28e71c742ccfbdb2c4e28609030c05ba
ChrisAkridas <xrhstosakridas@gmail.com>
2025-04-24 16:34:53 +0300

    feat: update python script for running BKT model

--- src/lib/scripts/python/bkt.py
+++ src/lib/scripts/python/bkt.py
@@ -4,6 +4,7 @@ import sys
 import json
 import numpy as np
 import pandas as pd
+import pickle
 from pyBKT.models import Model
 
 # for Linux replace "\\" with "/"
@@ -12,26 +13,36 @@ my_path = os.path.dirname(__file__)
 platform = platform.system()
 divider = "\\" if platform == "Windows" else "/"
 
-def train_pybkt_model(): 
-    model = Model()
-    model.fit(data_path= my_path + divider + "trainingDataset.csv")
+# def train_pybkt_model(): 
+#     model = Model()
+#     model.fit(data_path= my_path + divider + "trainingDataset.csv")
 
 def save_model(model, file_path): 
     model.save(file_path)
 
-def load_model(file_path):
-    return Model.load(file_path)
+def load_model(filePath):
+    try: 
+        if not os.path.exists(filePath):
+            print(f"File {filePath} does not exist.")
+            return None
+
+        with open(filePath, 'rb') as file:
+            model = pickle.load(file)
+        
+        print(f"Model successfully loaded from {filePath}")
+        return model
+       
+    except Exception as e:
+        print(f"Error loading pyBKT model: {e}")
+        return None
 
 def train_model():
     model = Model()
     model.fit(data_path=my_path + divider + "trainingDataset.csv")
+    save_model(model, my_path + divider + "model.pkl")
     return model
 
-def run_bkt():
-    model = Model()
-    
-    model.fit(data_path = my_path + divider + 'trainingDataset.csv',)  # Replace with actual dataset
-    save_model(model, my_path + divider + 'model.pkl')
+def run_bkt(model):
     predictions = model.predict(data_path = my_path + divider + 'chrisData.csv')
     evaluations = model.evaluate(data_path = my_path + divider +'chrisData.csv')
     return {"predictions": predictions, "evaluations": evaluations}
@@ -39,6 +50,12 @@ def run_bkt():
 if __name__ == "__main__":
     input_data = json.loads(sys.argv[1])
     # skills = input_data.get('skills')
+    print(f"Input data: {input_data}")
     
-    result = run_bkt()
-    print(result)
+    model = load_model(my_path + divider + 'model.pkl')
+    if model is None: 
+       model = train_model()
+
+    result = run_bkt(model)
+    train_model()
+    print('model: ', model, result)
